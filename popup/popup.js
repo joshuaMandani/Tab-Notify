f6a8a9d64b4a0b380fd8282226a3638143c46ea0
Joshua Mandani <joshuamandani17@gmail.com>
2024-07-18 07:44:14 -0400

    Format documents and remove debugging lines

--- popup/popup.js
+++ popup/popup.js
@@ -1,52 +1,55 @@
 async function is_tracked() {
-    const response = await chrome.runtime.sendMessage({message: "is_tracked"});
+  const response = await chrome.runtime.sendMessage({
+    message: "is_tracked",
+  });
 
-    let url = new URL(response.tab.url);
+  let url = new URL(response.tab.url);
 
+  if (url.href.length > url.origin.length) {
+    document.querySelector(".link").innerHTML = `${url.origin} [...]`;
+  } else {
+    document.querySelector(".link").innerHTML = url.origin;
+  }
+  document.querySelector(".title").innerHTML = response.tab.title;
+  document.querySelector(".favIconUrl").src = response.tab.favIconUrl;
 
-    if (url.href.length > url.origin.length) {
-        document.querySelector(".link").innerHTML = `${url.origin} [...]`;
-    } else {
-        document.querySelector(".link").innerHTML = url.origin;
-
-    }
-    document.querySelector(".title").innerHTML = response.tab.title;
-    document.querySelector(".favIconUrl").src = response.tab.favIconUrl;
-
-    if (response.tracked) {
-        document.querySelector("#add").innerHTML = "Remove current tab";
-    } else {
-        document.querySelector("#add").innerHTML = "Add current tab";
-    }
-    return response.tracked;
+  if (response.tracked) {
+    document.querySelector("#add").innerHTML = "Remove current tab";
+  } else {
+    document.querySelector("#add").innerHTML = "Add current tab";
+  }
+  return response.tracked;
 }
 
 async function track_tab() {
-    const response = await chrome.runtime.sendMessage({message: "add_tab"});
+  const response = await chrome.runtime.sendMessage({
+    message: "add_tab",
+  });
+
+  if (response.action == "added") {
     document.querySelector("#add").innerHTML = "Remove current tab";
+  }
 }
 
 async function remove_tab() {
-    const response = await chrome.runtime.sendMessage({message: "remove_tab"});
+  const response = await chrome.runtime.sendMessage({
+    message: "remove_tab",
+  });
+  if (response.action == "removed") {
     document.querySelector("#add").innerHTML = "Add current tab";
+  }
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-    let tracked = await is_tracked();
+  let tracked = await is_tracked();
 
-    document.querySelector('#add').addEventListener('click', function () {
-        if (tracked) {
-            remove_tab();
-        } else {
-            track_tab();
-            
-        }
+  document.querySelector("#add").addEventListener("click", () => {
+    if (tracked) {
+      remove_tab();
+    } else {
+      track_tab();
+    }
 
-        tracked = !tracked;
-
-    })
-
-
-
-})
-
+    tracked = !tracked;
+  });
+});
--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,19 @@
 let tracked_tab_ids = [];
 let current_tab = {
-  "favIconUrl": "",
-  "id": -999999999,
-  "status": "",
-  "title": "",
-  "url": "",
+  favIconUrl: "",
+  id: -999999999,
+  status: "",
+  title: "",
+  url: "",
 };
 let count = 0;
 
 async function getCurrentTab() {
   console.log("Gathering current tab information");
-  let queryOptions = { active: true, lastFocusedWindow: true };
+  let queryOptions = {
+    active: true,
+    lastFocusedWindow: true,
+  };
 
   let [tab] = await chrome.tabs.query(queryOptions);
 
@@ -19,7 +22,6 @@ async function getCurrentTab() {
   current_tab.status = tab?.status;
   current_tab.title = tab?.title;
   current_tab.url = tab?.url || tab?.pendingUrl;
-
 }
 
 function track_tab(tab_id) {
@@ -29,7 +31,7 @@ function track_tab(tab_id) {
 
 function remove_tab(tab_id) {
   if (tracked_tab_ids.indexOf(tab_id) == -1) {
-    return
+    return;
   }
 
   tracked_tab_ids.splice(tracked_tab_ids.indexOf(tab_id), 1);
@@ -44,21 +46,17 @@ chrome.runtime.onStartup.addListener(() => {
   getCurrentTab();
 });
 
-
 chrome.tabs.onUpdated.addListener(() => {
   getCurrentTab();
 });
-
 
 chrome.tabs.onHighlighted.addListener(() => {
   getCurrentTab();
 });
 
-
 chrome.tabs.onRemoved.addListener((tabId) => {
   remove_tab(tabId);
 });
-
 
 chrome.windows.onFocusChanged.addListener((windowId) => {
   if (windowId !== -1) {
@@ -66,33 +64,24 @@ chrome.windows.onFocusChanged.addListener((windowId) => {
   }
 });
 
-
-chrome.runtime.onMessage.addListener(
-   function(request, sender, sendResponse) {
-    if (request.message == "is_tracked") {
-      if (tracked_tab_ids.indexOf(current_tab.id) == -1) {
-        sendResponse({tracked: false, tab: current_tab});
-      } else {
-        sendResponse({tracked: true, tab: current_tab});
-      }
-
-
-    } else if (request.message == "add_tab") {
-      console.log(current_tab.id);
-
-      track_tab(current_tab.id);
-
-      console.log('Tracked IDs is' + tracked_tab_ids);
-
-      sendResponse({action: "added"})
-    } else if (request.message == "remove_tab") {
-      console.log(current_tab.id);
-
-      remove_tab(current_tab.id);
-
-      console.log('Tracked IDs is' + tracked_tab_ids);
-      sendResponse({action: "removed"})
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message == "is_tracked") {
+    if (tracked_tab_ids.indexOf(current_tab.id) == -1) {
+      sendResponse({
+        tracked: false,
+        tab: current_tab,
+      });
+    } else {
+      sendResponse({
+        tracked: true,
+        tab: current_tab,
+      });
     }
-
+  } else if (request.message == "add_tab") {
+    track_tab(current_tab.id);
+    sendResponse({ action: "added" });
+  } else if (request.message == "remove_tab") {
+    remove_tab(current_tab.id);
+    sendResponse({ action: "removed" });
   }
-);
+});
